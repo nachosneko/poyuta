29e632c5fa3dde97adf1b3a0259ac2d8c388e8dc
nachosneko <hhhusah@gmail.com>
2023-11-01 20:07:33 +0100

    removed old functions and added new ones

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,13 @@
-# third party imports
 import discord
+from discord import app_commands
 from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.ext import tasks
+import random
+import asyncio
+import sqlite3
 
-# Internal imports
 from utils import load_environment
-
 
 config = load_environment()
 
@@ -12,113 +15,77 @@ intents = discord.Intents.all()
 intents.reactions = True
 intents.messages = True
 
-bot = commands.Bot(command_prefix="err ", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Define a dictionary to store reaction roles
-reaction_roles = {
-    "❤️": 1168130056497942549,  # Emote and Role ID for the first role
-    "👍": 1168196460324593744,  # Emote and Role ID for the second role
-    # You can initially set up some default emotes and roles
-}
 
+# Store game state (current female clip and correct answer)
+current_female_clip = "none yet..."
+current_male_clip = "none yet..."
+correct_female = "?"
+correct_male = "?"
+admin_user_ids = [195534572581158913]  # Replace with the admin user IDs
+
+
+# Function to change the quiz audio and correct answer
+async def change_female(new_female_clip, new_correct_female):
+    global current_female_clip, correct_female
+    current_female_clip = new_female_clip
+    correct_female = new_correct_female
+
+async def change_male(new_male_clip, new_correct_male):
+    global current_male_clip, correct_male
+    current_male_clip = new_male_clip
+    correct_male = new_correct_male
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}")
-
-
-@bot.event
-async def on_raw_reaction_add(payload):
-    if payload.guild_id == 366643056138518529 and str(payload.emoji) in reaction_roles:
-        role_id = reaction_roles[str(payload.emoji)]
-        role = discord.utils.get(payload.member.guild.roles, id=role_id)
-        await payload.member.add_roles(role)
-
-
-@bot.event
-async def on_raw_reaction_remove(payload):
-    if payload.guild_id == 366643056138518529 and str(payload.emoji) in reaction_roles:
-        role_id = reaction_roles[str(payload.emoji)]
-        role = discord.utils.get(bot.get_guild(payload.guild_id).roles, id=role_id)
-        member = bot.get_guild(payload.guild_id).get_member(payload.user_id)
-        await member.remove_roles(role)
-
-
-@bot.command()
-async def roles(ctx):
-    # Create an embed
-    embed = discord.Embed(
-        title="Quiz Roles",
-        description="React to get a role:",
-        color=0xBBE6F3,  # You can set the color of the embed here
-    )
-
-    for emote, role_id in reaction_roles.items():
-        role = ctx.guild.get_role(role_id)
-        if role:
-            embed.add_field(name=emote, value=f"<@&{role_id}>", inline=True)
-
-    embed.add_field(
-        name="Additional Information",
-        value="You can add more details here.",
-        inline=False,
-    )
-
-    embed.set_author(name="nayuta", icon_url="https://i.imgur.com/lR3Gshd.png")
-
-    embed.set_footer(text=" --- ", icon_url="https://i.imgur.com/0NSQW44.png")
-
-    embed.set_image(url="https://i.imgur.com/QeMw2PV.png")
-
-    message = await ctx.send(embed=embed)
-
-    for emote in reaction_roles:
-        await message.add_reaction(emote)
-
-
-@bot.command()
-async def addrole(ctx, emote, role: discord.Role):
-    # Check if the user has permission to add reaction roles
-    if not ctx.author.guild_permissions.manage_roles:
-        await ctx.send("You don't have the required permissions to add reaction roles.")
-        return
-
-    # Check if the emote is valid
+    print(f"logged in as {bot.user.name}")
     try:
-        await ctx.message.add_reaction(emote)
+        synced = await bot.tree.sync()
+        print(f"synced {len(synced)} command(s)")
     except Exception as e:
-        await ctx.send(f"Invalid emote: {e}")
-        return
+        print(e)
 
-    # Add the emote and role to the dictionary
-    reaction_roles[emote] = role.id
-    await ctx.send(f"Reaction role added: {emote} -> {role.mention}")
-
-
-# Define a list of correct answers
-correct_answers = ['correct answer 1', 'correct answer 2']
-
-@bot.event
-async def on_message(message):
-    if message.channel.id == config["CHANNEL_OR_THREAD_ID"] and '||' in message.content:
-        # Process the user's input
-        await process_user_input(message)
-    await bot.process_commands(message)
-
-
-async def process_user_input(message):
-    user_response = message.content[len('||'):]  # Remove the catchphrase prefix
-    await check_and_react(user_response, message)
-
-async def check_and_react(user_response, message):
-    user_response = user_response.lower()
-    user_response = user_response.replace("||", "").strip()
-    
-    if any(answer in user_response for answer in correct_answers):
-        await message.add_reaction("✅")  # React with a correct emote
+@bot.command()
+async def currentclips(ctx):
+    if current_female_clip:
+        await ctx.send(current_female_clip)
+    if current_male_clip:
+        await ctx.send(current_male_clip)
     else:
-        await message.add_reaction("❌")  # React with an incorrect emote
+        await ctx.send("no clips in progress")
 
+@bot.command()
+async def newfemale(ctx, new_female_clip, new_correct_female):
+    if ctx.author.id not in admin_user_ids:
+        await ctx.send("only admins can change the clip")
+    else:
+        await change_female(new_female_clip, new_correct_female)
+        await ctx.send(f"female clip updated")
 
-# Run your bot with your bot token
+@bot.command()
+async def newmale(ctx, new_male_clip, new_correct_male):
+    if ctx.author.id not in admin_user_ids:
+        await ctx.send("only admins can change the clip")
+    else:
+        await change_male(new_male_clip, new_correct_male)
+        await ctx.send(f"male clip updated")
+
+@bot.tree.command(name="female")
+@app_commands.describe(seiyuu_female ="guess the female seiyuu")
+async def female(interaction: discord.Interaction, seiyuu_female: str):
+    if seiyuu_female.lower() == correct_female.lower():
+        await interaction.response.send_message(f"you guessed it **correctly** :muscle:")
+    else:
+        await interaction.response.send_message(f"**incorrect** :skull:")
+
+@bot.tree.command(name="male")
+@app_commands.describe(seiyuu_male ="guess the male seiyuu")
+async def male(interaction: discord.Interaction, seiyuu_male: str):
+    if seiyuu_male.lower() == correct_male.lower():
+        await interaction.response.send_message(f":fearful: you guessed it **correctly**")
+    else:
+        await interaction.response.send_message(f"**incorrect** :skull:")
+
+# Run the bot with your token
 bot.run(config["BOT_SECRET_TOKEN"])
--- bot/utils.py
+++ bot/utils.py
@@ -30,3 +30,195 @@ def load_environment() -> dict:
     }
 
     return config
+
+import re
+
+
+ANIME_REGEX_REPLACE_RULES = [
+    # Ļ can't lower correctly with sql lower function
+    {"input": "ļ", "replace": "[ļĻ]"},
+    {"input": "l", "replace": "[l˥ļĻΛ]"},
+    # Ź can't lower correctly with sql lower function
+    {"input": "ź", "replace": "[źŹ]"},
+    {"input": "z", "replace": "[zźŹ]"},
+    {"input": "ou", "replace": "(ou|ō|o)"},
+    {"input": "oo", "replace": "(oo|ō|o)"},
+    {"input": "oh", "replace": "(oh|ō|o)"},
+    {"input": "wo", "replace": "(wo|o)"},
+    {"input": "o", "replace": "([oōóòöôøӨΦο]|ou|oo|oh|wo)"},
+    {"input": "uu", "replace": "(uu|u|ū)"},
+    {"input": "u", "replace": "([uūûúùüǖμ]|uu)"},
+    {"input": "aa", "replace": "(aa|a)"},
+    {"input": "ae", "replace": "(ae|æ)"},
+    {"input": "a", "replace": "([aäãά@âàáạåæā∀Λ]|aa)"},
+    {"input": "c", "replace": "[cςč℃Ↄ]"},
+    # É can't lower correctly with sql lower function
+    {"input": "é", "replace": "[éÉ]"},
+    {"input": "e", "replace": "[eəéÉêёëèæē]"},
+    {"input": "'", "replace": "['’ˈ]"},
+    {"input": "n", "replace": "[nñ]"},
+    {"input": "0", "replace": "[0Ө]"},
+    {"input": "2", "replace": "[2²]"},
+    {"input": "3", "replace": "[3³]"},
+    {"input": "5", "replace": "[5⁵]"},
+    {"input": "*", "replace": "[*✻＊✳︎]"},
+    {
+        "input": " ",
+        "replace": "( ?[²³⁵★☆♥♡\\/\\*✻✳︎＊'ˈ-∽~〜・·\\.,;:!?@_-⇔→≒=\\+†×±◎Ө♪♩♣␣∞] ?| )",
+    },
+    {"input": "i", "replace": "([iíίɪ]|ii)"},
+    {"input": "x", "replace": "[x×]"},
+    {"input": "b", "replace": "[bßβ]"},
+    {"input": "r", "replace": "[rЯ]"},
+    {"input": "s", "replace": "[sς]"},
+]
+
+
+def escape_and_replace(input_str: str) -> str:
+    """Escape special characters and replace specific sequences.
+
+    Parameters
+    ----------
+    input_str : str
+        String to escape and replace.
+
+    Returns
+    -------
+    str
+        Escaped and replaced string.
+    """
+
+    escaped_str = re.escape(input_str)
+    escaped_str = escaped_str.replace(r"\ ", " ")
+    escaped_str = escaped_str.replace(r"\*", "*")
+    return escaped_str
+
+
+def apply_regex_rules(input_str: str) -> str:
+    """Apply replacement rules using compiled regular expressions.
+
+    Parameters
+    ----------
+    input_str : str
+        String to apply replacement rules to.
+
+    Returns
+    -------
+    str
+        String with replacement rules applied.
+    """
+
+    output_str = input_str
+    for rule in ANIME_REGEX_REPLACE_RULES:
+        pattern = re.compile(re.escape(rule["input"]), re.IGNORECASE)
+        output_str = pattern.sub(rule["replace"], output_str)
+    return output_str
+
+
+def generate_regex_pattern(input_str: str, partial_match: bool = True) -> str:
+    """Generate a regex pattern for a string.
+    Uses the rules defined in ANIME_REGEX_REPLACE_RULES.
+
+    Parameters
+    ----------
+    input_str : str
+        String to generate regex pattern for.
+
+    partial_match : bool, optional
+        Whether to match the whole string or not, by default True
+
+    Returns
+    -------
+    str
+        Regex pattern.
+    """
+
+    # Escape and replace special characters
+    input_str = escape_and_replace(input_str.lower())
+
+    # Apply replacement rules
+    ouput_str = apply_regex_rules(input_str)
+
+    # Allow partial match or not
+    ouput_str = f".*{ouput_str}.*" if partial_match else f"^{ouput_str}$"
+
+    return ouput_str
+
+
+def process_user_input(
+    input_str: str, partial_match: bool = True, swap_words: bool = True
+) -> str:
+    """Generate a regex pattern for a string.
+    Uses the rules defined in ANIME_REGEX_REPLACE_RULES.
+
+    Parameters
+    ----------
+    input_str : str
+        String to generate regex pattern for.
+
+    partial_match : bool, optional
+        Whether to match the whole string or not, by default True
+
+    swap_words : bool, optional
+        Whether to allow to swap the order of the words or not, by default True
+        Will allow to swap the order of the words if there are exactly two words.
+
+    Returns
+    -------
+    str
+        Regex pattern.
+    """
+
+    # Generate the regex pattern
+    output_str = generate_regex_pattern(input_str, partial_match=partial_match)
+
+    # if swap_words is False, or there isn't exactly two words, return the pattern
+    if not swap_words or len(input_str.split(" ")) != 2:
+        return output_str
+
+    # else generate the pattern for the swapped user input, and return the pattern combined with the original pattern
+    swapped_input_str = " ".join(input_str.split(" ")[::-1])
+    swapped_output_str = generate_regex_pattern(
+        swapped_input_str, partial_match=partial_match
+    )
+    output_str = f"({output_str})|({swapped_output_str})"
+
+    return output_str
+
+
+def extract_answer_from_user_input(user_input: str) -> str:
+    """Extract the answer from a user input.
+    The answer is the part of the user input, which start with either
+    `female` or `male` and is inbetween the two `||`.
+
+    Parameters
+    ----------
+    user_input : str
+        User input.
+
+    Returns
+    -------
+    str
+        The extracted answer or None if no answer is found.
+
+    Examples
+    --------
+    - `female ||Saori Hayami||` -> `Saori Hayami`
+    - `male ||Yuuichi Nakamura||` -> `Yuuichi Nakamura`
+    - `blah blah` -> None
+    """
+
+    # Define a pattern to search for the answer
+    pattern = re.compile(r"(female|male)\s+\|\|(.+?)\|\|")
+
+    # Use the pattern to search for matches in the message
+    match = pattern.search(user_input)
+
+    # Return None if no match is found
+    if not match:
+        return None
+
+    # Otherwise, return the "answer" part
+    return match.group(2)
+
+
